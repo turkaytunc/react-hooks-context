e799afda20806d8daa0eee79eb47d62d4625e7d2
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-24 18:03:16 +0300

    Added navbar-mock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
