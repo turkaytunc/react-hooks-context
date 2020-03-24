36a75d7406cd9d1acfb9654a3b9c0fa2ec775756
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-24 18:19:35 +0300

    added nav and list css

--- src/components/booklist/BookList.js
+++ src/components/booklist/BookList.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import "./book-list.css";
 
 export class BookList extends Component {
   render() {
     return (
       <div className="book-list">
         <ul>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
+          <li>item1</li>
+          <li>item2</li>
+          <li>item3</li>
         </ul>
       </div>
     );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./navbar.css";
 
 export default class Navbar extends Component {
   render() {
