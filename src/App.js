880cbfd4369cef4a6e25eb3284d023bd308996ee
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-24 18:07:00 +0300

    Added list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import BookList from "./components/booklist/BookList";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <BookList></BookList>
     </div>
   );
 }
---
+++ src/components/booklist/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
